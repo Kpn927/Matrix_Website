7feaf63eb6b0a560899d59633952f5008dc132ed
Daniel Sosa <61805771+Kpn927@users.noreply.github.com>
2024-11-13 20:18:54 -0400

    CHORE: documentación y entendimiento

--- matrices/script.js
+++ matrices/script.js
@@ -6,18 +6,15 @@ class MatrizHandler{
 
     // INGRESAR //
     ingresarMatriz(matrizNum) {
-
         // El 10 en parseINT es para definir la báse númerica que se va a utilizar, ya sea binario (2), hexadecimal (16) o Decimal (10)
         const filas = parseFloat(document.getElementById(`filas${matrizNum}`).value, 10);
         const columnas = parseFloat(document.getElementById(`columnas${matrizNum}`).value, 10);
-        if (!filas || !columnas) {
-            alert("Por favor, ingresa las medidas.");
-            return;
-        }
         let matriz = [];
 
         for (let i = 0; i < filas; i++) {
             let fila = prompt(`Introduce los elementos de la fila ${i + 1} para la matriz ${matrizNum}, separados por comas:`);
+            // MAP y NUMBER convierte los números ingresados a números reales, en ejemplo 
+            //["1","2","3"] A [1, 2, 3]
             let elementos = fila.split(',').map(Number);
             if (elementos.length !== columnas) {
                 alert(`Por favor, introduce exactamente ${columnas} elementos.`);
@@ -132,6 +129,29 @@ class MatrizHandler{
         this.visualizarMatriz(resultado, 'resultTable');
     }
 
+    // LIMPIAR // 
+
+    limpiarMatrices() {
+
+        if (this.matriz1.length === 0 && this.matriz2.length === 0) {
+            alert('No hay matrices por limpiar');
+        } else {
+            this.matriz1 = [];
+            this.matriz2 = [];
+            document.getElementById('matrixTable1').innerHTML = '';
+            document.getElementById('matrixTable2').innerHTML = '';
+            document.getElementById('resultTable').innerHTML = '';
+
+            document.getElementById('filas1').value = '';
+            document.getElementById('columnas1').value = '';
+            document.getElementById('filas2').value = '';
+            document.getElementById('columnas2').value = '';
+            document.getElementById('escalarNUM').value = '';
+
+            alert('Matrices limpiadas y campos reiniciados.');
+        }
+    }
+
     // INVERSA //
     obtenerInversa(matriz) {
 
@@ -140,12 +160,17 @@ class MatrizHandler{
             return;
         }
         const n = matriz.length;
+        //Realiza una copia exacta el array matriz.
         let mat = JSON.parse(JSON.stringify(matriz));
+        // Se utiliza _ para ignorar el primer parametro del callback
+        // Básicamente pregunta si I = J pone un 1 si no, pone un 0
+        // y para cada fila i crea un array de longitud n, que n es la longitud de la matriz
         let inv = Array.from({ length: n }, (_, i) =>
             Array.from({ length: n }, (_, j) => (i === j ? 1 : 0))
         );
-
-        for (let i = 0; i < n; i++) {
+        
+        // acá lo que hace es encontrar el valor absoluto de más grande de la columna actual y la marca para ser intercambiada.
+        for (let i = 0; i < n; i++) {   
             let maxRow = i;
             for (let k = i + 1; k < n; k++) {
                 if (Math.abs(mat[k][i]) > Math.abs(mat[maxRow][i])) {
@@ -153,22 +178,25 @@ class MatrizHandler{
                 }
             }
 
+            //Ocurre un intercambio de filas.
             [mat[i], mat[maxRow]] = [mat[maxRow], mat[i]];
             [inv[i], inv[maxRow]] = [inv[maxRow], inv[i]];
 
             if (mat[i][i] === 0) {
-                alert("La matriz no tiene inversa");
-                return;
+                throw new Error("La matriz no tiene inversa");
             }
 
+            //se inicializa el pivote y se divide por cada elemento de la fila
             const pivot = mat[i][i];
             for (let j = 0; j < n; j++) {
                 mat[i][j] /= pivot;
                 inv[i][j] /= pivot;
             }
 
+            //Básicamente hace que todos los elementos de la columna i excepto el pivote sean 0
             for (let k = 0; k < n; k++) {
                 if (k !== i) {
+                    //se obtiene el valor que se busca eliminar de la columna i de la fila k 
                     const factor = mat[k][i];
                     for (let j = 0; j < n; j++) {
                         mat[k][j] -= factor * mat[i][j];
@@ -180,49 +208,21 @@ class MatrizHandler{
         this.visualizarMatriz(inv, 'resultTable');
     }
 
-
-    // Producto Escalar
-    productoEscalar(matriz) {
-        
-        if (!matriz.length) {
-            alert("Ingrese una matriz primero.")
-            return;
-        }
-        
-        const resultado = [];
-        const Escalar = parseInt(document.getElementById(`escalarNUM`).value, 10);
-
-        if (!Escalar) {
-            alert("Ingrese la variable a escalar. ");
-            return;
-        }
-        
-        for (let i = 0; i < matriz.length; i++) {
-            const fila = [];
-            for (let j = 0; j < matriz[i].length; j++) {
-                fila.push(matriz[i][j] * Escalar);
-            }
-            resultado.push(fila);
-        }
-    
-        this.visualizarMatriz(resultado, 'resultTable');
-    }
-
     // Transpuesta
     transponerMatriz(matriz) {
         // Verifica si la matriz está vacía
         if (!matriz.length) {
-            alert("Ingrese una matriz primero.")
+            prompt("Ingrese una matriz primero.")
             return;
         }
-        
+      
         // Obtén el número de filas y columnas de la matriz original
         const filas = matriz.length;
         const columnas = matriz[0].length;
-        
+      
         // Inicializa una matriz vacía para almacenar la transpuesta
         let resultado = [];
-        
+      
         // Itera sobre las columnas de la matriz original
         for (let j = 0; j < columnas; j++) {
           // Inicializa una nueva fila en la matriz transpuesta
@@ -231,97 +231,84 @@ class MatrizHandler{
           for (let i = 0; i < filas; i++) {
             // Copia el elemento de la matriz original a la posición correspondiente en la transpuesta
             resultado[j][i] = matriz[i][j];
-        }
+            }
         }
         this.visualizarMatriz(resultado, 'resultTable');
     }
     
+    // Este método divide cada elemento de la fila row por un valor value.
     divideRow(row, value) {
         return row.map(item => item / value)
     }
-    
+
+    // Lo mismo que el de arriba solo que con un *
     multiplyRow(row, value) {
         return row.map(item => item * value)
     }
-    
+
+    // Este método suma dos filas row1 y row2. Opcionalmente, puede invertir (cambiar el signo) de los elementos de cada fila antes de sumarlos.
+    // invert1 ? -1 : 1 y invert2 ? -1 : 1: Estas expresiones determinan si se debe invertir (multiplicar por -1) los elementos de row1 y row2 respectivamente.
     addRows(row1, row2, invert1, invert2) {
         let row3 = []
     
         row3 = row1.map((item, i) => {
-            return row1[i] * (invert1 ? -1 : 1) + row2[i] * (invert2 ? -1 : 1)
+          return row1[i] * (invert1 ? -1 : 1) + row2[i] * (invert2 ? -1 : 1)
         })
     
         return row3
     }
 
-    solveByGaussJordan(matriz, i = 0) {
-        if (i == matriz.length) {
-          return fixedPrecisionMatrix(matriz)
+    GaussJordan(matrix, i = 0) {
+        // Verifica si i (el índice actual de la fila) es igual a la longitud de la matriz.
+        if (i == matrix.length) {
+          return fixedPrecisionMatrix(matrix)
         }
-    
-        let m = [...matriz];
+        
+        // Creamos una copia de la matriz.
+        let m = [...matrix];
+        // Selecciona la fila actual currentRow y el elemento pivote pivot, que es el elemento en la posición [i][i].
         let currentRow = m[i];
         let pivot = currentRow[i];
-    
+        
+        // Normaliza la fila actual dividiéndola por el pivote, de modo que el pivote se convierta en 1.
         m[i] = this.divideRow(currentRow, pivot)
         
+        // Si la fila es la actual (mapIndex == i): La deja sin cambios.
+        //Si la fila no es la actual La actualiza restando un múltiplo de la fila pivote para hacer que los elementos en la columna i sean cero.
+
         m = m.map((item, mapIndex) => {
-        if (mapIndex == i) { // ignore already processed row
+          if (mapIndex == i) {
             return item
-        } else {
+          } else {
             return this.addRows(this.multiplyRow(m[i], -item[i]), item)
-        }
+          }
         })
+        return this.GaussJordan(m, i + 1);
+    }
+    
+    // Funcion para ver la matriz resuelta no mas
+    visualizarMatrizIdentidad(matrix){
+        let solvedMatrix = this.GaussJordan(matrix);
+        this.visualizarMatriz(solvedMatrix, 'resultTable');
+    }
+      
 
-        return this.solveByGaussJordan(m, i + 1);
-    }
-    
-    
-    visualizarMatrizIdentidad(matriz){
-        if (!matriz.length) {
-            alert("Ingrese una matriz primero.")
-            return;
-        }
-        let solvedMatriz = this.solveByGaussJordan(matriz);
-        if (!solvedMatriz[1][1]) {
-            alert("La matriz no tiene matriz identidad. ");
-            return;
-        }
-        this.visualizarMatriz(solvedMatriz, 'resultTable');
-    }
-
-    // Limpiar //
-    limpiarMatrices() {
-         
-        if (this.matriz1.length === 0 && this.matriz2.length === 0 && escalarNUM == 0) {
-            alert('No hay matrices por limpiar');
-        } else {
-            this.matriz1 = [];
-            this.matriz2 = [];
-            document.getElementById('matrixTable1').innerHTML = '';
-            document.getElementById('matrixTable2').innerHTML = '';
-            document.getElementById('resultTable').innerHTML = '';
-    
-            document.getElementById('filas1').value = '';
-            document.getElementById('columnas1').value = '';
-            document.getElementById('filas2').value = '';
-            document.getElementById('columnas2').value = '';
-            document.getElementById('escalarNUM').value = '';
-    
-            alert('Matrices limpiadas y campos reiniciados.');
-        }
-    }
 }
-
+// Esta función toma un número n y lo convierte en un número con una cantidad fija de decimales, luego lo retorna como un número en vez de una cadena.
 const short = (n, decimals = 3) => {
     return Number(n.toFixed(decimals))
 }
 
+// Esta función toma una matriz m y redondea el último elemento de cada fila a 8 decimales.
 const fixedPrecisionMatrix = m => {
+    //aplica la función a cada array de la matriz.
     return m.map(item => (
       [
+        //remueve todos los elementos de la fila excepto el ultimo 
         ...item.splice(0, item.length - 1),
+        //Rendondea el úlitmo a 8 decimales.
         short(item[item.length - 1], 8)
+        //... pasa todo los elementos removidos y el elemento redondeado a una nueva fila.
       ]
     ))
 }
